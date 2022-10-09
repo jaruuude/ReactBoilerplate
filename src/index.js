97d307ffcb44140a33937be092992a8c31b1ba3b
jaruuude <jaredrclarke@outlook.co.nz>
2022-10-10 11:59:42 +1300

    first push

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// key react dependencies
+import ReactDOM from 'react-dom/client';
+import './styles.css';
+// import our components
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+
+  </>
+);
---
+++ src/utilities/add.js
@@ -0,0 +1,9 @@
+const addTwoNumbers = () => {
+    const num1 = 2
+    const num2 = 5
+    console.log(
+        num1 + num2
+    )
+}
+
+export default addTwoNumbers
